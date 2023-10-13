90dad343ea2610ac1c9579587f7b4487161d13ea
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-10-13 12:30:55 +0900

    チャットアプリ完成？

--- 01/NT1_01_Chat/NT1_01_Client/main.cpp
+++ 01/NT1_01_Chat/NT1_01_Client/main.cpp
@@ -1,20 +1,83 @@
-﻿// NT1_01_Client.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-#include <iostream>
+﻿#include <winsock2.h> /* WinSockのヘッダファイル */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SOCKET s;
+	SOCKADDR_IN saddr;
+	u_short uport;
+	char szServer[1024] = { 0 };
+	HOSTENT* lpHost;
+	unsigned int addr;
+
+	/* ポート番号の入力 */
+	printf("使用するポート番号 --> ");
+	scanf_s("%hd", &uport);
+	fflush(stdin);
+
+	/* サーバ名またはサーバのIPアドレスを入力 */
+	printf("IPアドレス -->");
+	scanf_s("%s", szServer, 1024);
+	fflush(stdin);
+
+	/* ソケットをオープン */
+	s = socket(AF_INET, SOCK_STREAM, 0);/*　☆socket関数　*/
+		if (s == INVALID_SOCKET) {
+			printf("ソケットオープンエラー\n");
+			return INVALID_SOCKET;
+		}
+
+	/* サーバーを名前で取得する */
+	lpHost = gethostbyname(szServer);
+	if (lpHost == NULL) {
+		/* サーバーをIPアドレスで取得する */
+		addr = inet_addr(szServer);
+		lpHost = gethostbyaddr((char*)&addr, 4, AF_INET);
+	}
+
+	if (lpHost == NULL) {
+		printf("Hostが見つかりません\n");
+		closesocket(s);
+		return NULL;
+	}
+
+	/*　☆スライドを参考にSOCKADDR_IN型変数「saddr」の設定　*/
+	memset(&saddr, 0, sizeof(SOCKADDR_IN));
+	saddr.sin_family = lpHost->h_addrtype;
+	saddr.sin_port = htons(uport);
+	saddr.sin_addr.s_addr = *((u_long*)lpHost->h_addr);
+
+	/*　☆connect関数を実行　*/
+	if ( connect(s,(SOCKADDR*)&saddr,sizeof(saddr)) == SOCKET_ERROR) {
+		printf("サーバーと接続できませんでした\n");
+		closesocket(s);
+		return SOCKET_ERROR;
+	}
+	printf("サーバーに接続できました\n");
+
+	while (1) {
+
+		/*　スライドを参考に送受信処理を入力　*/
+		int nRcv;
+		char szBuf[1024];
+
+		printf("送信-->");
+		scanf_s("%s", szBuf, 1024);
+		fflush(stdin);
+
+		send(s, szBuf, (int)strlen(szBuf), 0);
+		
+		nRcv = recv(s, szBuf, sizeof(szBuf) - 1, 0);
+		szBuf[nRcv] = '\0';
+		printf("受信--> %s\n", szBuf);
+		
+	}
+
+	/* ソケットを閉じる */
+	closesocket(s);
+	WSACleanup();
+
+    return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
--- 01/NT1_01_Chat/NT1_01_Server/main.cpp
+++ 01/NT1_01_Chat/NT1_01_Server/main.cpp
@@ -1,6 +1,98 @@
-﻿#include <iostream>
+﻿#include <winsock2.h> /* WinSockのヘッダファイル */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SOCKET listen_s;
+	SOCKET s;
+
+	SOCKADDR_IN from;
+	int fromlen;
+	u_short uport;
+
+	/* ポート番号の入力 */
+	printf("使用するポート番号--> ");
+	scanf_s("%hd", &uport);
+	fflush(stdin);
+
+	/* リスンソケットをオープン */
+	listen_s = socket(AF_INET, SOCK_STREAM, 0); /*　☆socket関数　*/
+	if (listen_s == INVALID_SOCKET) {
+		printf("リスンソケットオープンエラー");
+		WSACleanup();
+		return;
+	}
+	printf("リスンソケットをオープンしました\n");
+
+	SOCKADDR_IN saddr;
+	/* ソケットに名前を付ける */
+	/*　☆スライドを参考にSOCKADDR_IN型変数「saddr」の設定　*/
+	memset(&saddr, 0, sizeof(SOCKADDR_IN));
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(uport);
+	saddr.sin_addr.s_addr = INADDR_ANY;
+
+	/*　☆bind関数を実行　*/
+	if (bind(listen_s,(SOCKADDR*)&saddr, sizeof(saddr)) == SOCKET_ERROR) {
+		printf("bindエラー");
+		closesocket(listen_s);
+		return SOCKET_ERROR;
+	}
+	printf("bind成功です");
+
+	/* クライアントからの接続待ちの状態にする */
+	/*　☆listen関数を実行　*/
+	if (listen(listen_s, 0) == SOCKET_ERROR) {
+		printf("listenエラー\n");
+		closesocket(listen_s);
+		return SOCKET_ERROR;
+	}
+	printf("listen成功です\n");
+
+	/* 接続を待機する */
+	printf("acceptで待機します\n");
+	fromlen = (int)sizeof(from);
+	/*　☆SOCKADDR_IN型変数「from」を使ってaccept関数を実行　*/
+	s = accept(listen_s, (SOCKADDR*)&from, (int*)sizeof(fromlen));
+		if (s == INVALID_SOCKET) {
+			printf("acceptエラー\n");
+			closesocket(listen_s);
+			return;
+		}
+
+	printf("%sが接続してきました\n", inet_ntoa(from.sin_addr));
+	printf("accepet関数成功です\n");
+
+	/* リスンソケットはもう不要 */
+	closesocket(listen_s);
+
+	/* 会話開始 */
+	printf("会話開始\n");
+
+	while (1) {
+
+		/*　☆スライドを参考に送受信処理を入力　*/
+		int nRcv;
+		char szBuf[1024];
+
+		nRcv = recv(s, szBuf, sizeof(szBuf) - 1, 0);
+		szBuf[nRcv] = '\0';
+
+		printf("受信--> %s\n", szBuf);
+		printf("送信-->");
+
+		scanf_s("%s", szBuf, 1024);
+		fflush(stdin);
+
+		send(s, szBuf, (int)strlen(szBuf), 0);
+	}
+
+	/* ソケットを閉じる */
+	closesocket(s);
+	WSACleanup();
+
+	return 0;
 }
